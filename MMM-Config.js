ff9f54f5360eef4a58c5100950be45aa18f05b3f
Christopher Fenner <CFenner@users.noreply.github.com>
2022-01-12 15:26:07 +0100

    fix: remove any dom elements for the module

---
+++ MMM-Config.js
@@ -0,0 +1,14 @@
+/* Magic Mirror
+ * Module: MagicMirror-Config-Module
+ *
+ * By Christopher Fenner https://github.com/CFenner
+ * MIT Licensed.
+ */
+Module.register('MMM-Config', {
+  // Default module config.
+  defaults: {},
+  // Override dom template.
+  getTemplate: function () {
+    return ''
+  },
+})
